7af7903d4d2f712bd8353a1ccb583221503afe0f
grochmal <grochmal@member.fsf.org>
2016-07-30 21:37:55 +0100

    removed the python path configuration, we will use pip install -e instead

--- hq-dw/conf/dev.py
+++ hq-dw/conf/dev.py
@@ -13,17 +13,6 @@ SECRET_KEY = 'aaaaaabbbbbbccccccddddddeeeeeeffffffgggggghhhhhhii'
 DEBUG = True
 ALLOWED_HOSTS = []
 
-# The app names need to be duplicated here because the development versions
-# reside in their own repositories.
-APP_REPOS = [
-    'hq-stage'
-,   'hq-warehouse'
-,   'hq-hotel-mart'
-]
-APPS_DIR = os.path.join(BASE_DIR, '..', '..', 'hq-apps')
-for app in APP_REPOS:
-    sys.path.append(os.path.join(APPS_DIR, app))
-
 DATABASES = {
     # The default database will hold all authentication
     # and session data, this way we do not need to
